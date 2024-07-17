e2ba1fc0dfc3042050afd36ce2ec7d0533d87025
richu <richu.pr9@gmail.com>
2024-07-17 12:07:13 +0530

    initial commit

---
+++ src/main/java/com/example/sampleon/demosampleone/DemosampleoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleon.demosampleone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosampleoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosampleoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleon/demosampleone/DemosampleoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleon.demosampleone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosampleoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
